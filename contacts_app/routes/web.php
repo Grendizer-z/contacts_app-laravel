86b799aec5c639537ac6cb73304b29ef4309b77b
Carlos Ospino <futbolaucard@gmail.com>
2025-10-03 15:21:34 -0500

    configurar las variables de entorno

--- contacts_app/routes/web.php
+++ contacts_app/routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ContactController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [UserController::class, 'login'])->name('login'); 
+
+Route::post('/login', [UserController::class, 'login'])->name('login.post'); 
+
+Route::get('/registrar', [UserController::class, 'registrar'])->name('register');
+
+Route::post('/registrar', [UserController::class, 'registrar'])->name('register.post');
+
+Route::post('/logout', [UserController::class, 'logout'])->name('logout'); 
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/usuarios', [UserController::class, 'index'])->name('users.index');
+
+    Route::resource('contactos', ContactController::class)->names([
+        'index' => 'contacts.index',    
+        'create' => 'contacts.create',  
+        'store' => 'contacts.store',    
+        'edit' => 'contacts.edit',      
+        'update' => 'contacts.update',  
+        'destroy' => 'contacts.destroy'
+    ])->except(['show']); 
 });
